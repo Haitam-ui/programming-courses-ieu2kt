6a2302ba44384e6c2eccbd6f0c06ec703cb36975
Haitam-ui <120042041+Haitam-ui@users.noreply.github.com>
2022-12-15 20:27:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-ieu2kt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-ieu2kt)
---
+++ index.ts
@@ -0,0 +1,38 @@
+let Mynumber1, MyNumber2, MyNumber3: number;
+
+Mynumber1 = parseInt(prompt('please enter the first number'));
+
+MyNumber2 = parseInt(prompt('please enter the second number'));
+
+MyNumber3 = Mynumber1 + MyNumber2;
+
+console.log(MyNumber3);
+
+let MyText: string;
+
+MyText = 'haaaaaaaaaaaalaaaa madriiiiiiiiid';
+
+MyText = MyText + ' i am the best in the world';
+
+console.log(MyText);
+
+let myNumber: number;
+
+myNumber = parseInt(prompt('please enter the number'));
+
+if (myNumber > 0) {
+  console.log(' the number positive');
+}
+
+console.log('the end');
+
+let mynumber: number;
+
+mynumber = parseInt(prompt('please enter the numbeeer'));
+
+if (mynumber > 0) {
+  console.log('the number positive');
+} else {
+  console.log('the number negative');
+}
+console.log('the end');
