b9c6a054d8f8bb94260d8d5929ab37b46a4c7f9d
Haitam-ui <120042041+Haitam-ui@users.noreply.github.com>
2022-12-18 15:46:55 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
 let myNumber1, myNumber2, result: number;
 
-myNumber1 = parseInt(prompt('please enter the first number '));
+myNumber1 = parseInt(prompt('please enter the first number'));
 
 myNumber2 = parseInt(prompt('please enter the second number'));
 
-result = myNumber1 * myNumber2;
-
-if (result == 0) {
-  console.log('THE TWO NUMBERS ARE MISSING');
-} else if (result > 0) {
-  console.log('HITTING THE TWO NUMBERS IS POSITIVE');
+if (myNumber1 > myNumber2) {
+  result = myNumber1 - myNumber2;
 } else {
-  console.log('HIT THE NUMBERS IS NEGATIVE');
+  result = myNumber2 - myNumber1;
 }
+console.log(result);
